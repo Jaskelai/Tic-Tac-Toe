f0318b481fe35816c24483434d38642971f77923
iapolly <ris0070082>
2019-05-16 15:49:37 +0300

    Ожидание + гифка, путь которой надо изменить

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/view/paint.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/wait.fxml"));
         primaryStage.setScene(new Scene(root, SIZE, SIZE));
         primaryStage.show();
     }
